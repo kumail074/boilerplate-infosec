90799a87d8d5f71d77042ed31befb6b630133711
kumail074 <kumail074@proton>
2024-04-02 00:06:20 +0500

    hide x-powered-by

--- myApp.js
+++ myApp.js
@@ -43,7 +43,7 @@ app.use(helmet.hidePoweredBy());
 
 
 
-/* 
+
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
@@ -56,4 +56,4 @@ let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
-*/
+
